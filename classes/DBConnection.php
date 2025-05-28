2a84994ea6dc0233e031a8a450206d2587da6c94
NisargaSavadatti <131450609+NisargaSavadatti@users.noreply.github.com>
2025-05-28 13:22:22 +0530

    Update DBConnection.php

--- classes/DBConnection.php
+++ classes/DBConnection.php
@@ -1,31 +1,10 @@
 <?php
-if(!defined('DB_SERVER')){
-    require_once("../initialize.php");
-}
-class DBConnection{
-
-    private $host = DB_SERVER;
-    private $username = DB_USERNAME;
-    private $password = DB_PASSWORD;
-    private $database = DB_NAME;
-    
+class DBConnection {
     public $conn;
-    
-    public function __construct(){
-
-        if (!isset($this->conn)) {
-            
-            $this->conn = new mysqli($this->host, $this->username, $this->password, $this->database);
-            
-            if (!$this->conn) {
-                echo 'Cannot connect to database server';
-                exit;
-            }            
-        }    
-        
-    }
-    public function __destruct(){
-        $this->conn->close();
+    function __construct() {
+        $this->conn = new mysqli('db', 'root', '', 'tourism_db');
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
+        }
     }
 }
-?>
